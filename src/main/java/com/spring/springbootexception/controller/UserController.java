fb7d0604aee3d6a59f7f7443ab48f62a346c43f8
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-06-17 00:59:29 +0100

    using SimpleMappingExceptionResolver to handle exceptions

--- src/main/java/com/spring/springbootexception/controller/UserController.java
+++ src/main/java/com/spring/springbootexception/controller/UserController.java
@@ -1,18 +1,18 @@
-package com.spring.springbootexception.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-@Controller
-public class UserController {
-
-    @RequestMapping("/add")
-    public String add(){
-        int num = 10 / 0;
-        return "add";
-    }
-
-
-}
+//package com.spring.springbootexception.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//@Controller
+//public class UserController {
+//
+//    @RequestMapping("/add")
+//    public String add(){
+//        int num = 10 / 0;
+//        return "add";
+//    }
+//
+//
+//}
